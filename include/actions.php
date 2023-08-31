5c8875e3ceaa61f14d7d53c876de35cb46f78a3d
Hogne Titlestad <ht@friendup.cloud>
2023-08-31 20:53:04 +0200

    More.

--- include/actions.php
+++ include/actions.php
@@ -4,4 +4,19 @@
 *  This file is licensed under the MIT license. © 2023 Friend Software Labs AS *
 *******************************************************************************/
 
+// Do some checks
+if( !file_exists( '../archive' ) )
+	die( '{"message":"Failed to access repository.","response":404"}' );
+
+if( $_REQUEST[ 'action' ] == 'list' )
+{
+	
+}
+else if( $_REQUEST[ 'action' ] == 'media' )
+{
+}
+else if( $_REQUEST[ 'action' ] == 'get' )
+{
+}
+
 ?>
