2e9e90cc5e41febe8154ac1acb0b7dde2d4afeb7
Hogne Titlestad <ht@friendup.cloud>
2023-08-31 20:00:00 +0200

    Add more.

--- html/index.php
+++ html/index.php
@@ -4,7 +4,21 @@
 *  This file is licensed under the MIT license. © 2023 Friend Software Labs AS *
 *******************************************************************************/
 
-$tpl = file_get_contents( '../templates/index.html' );
+// Include the config
+if( file_exists( '../config.php' ) )
+	include( '../config.php' );
+
+// Web actions
+if( $_REQUEST[ 'action' ] )
+{
+	include( '../include/actions.php' );
+}
+
+// Get base template or custom template
+if( isset( $Config->index_template ) )
+	$tpl = file_get_contents( '../' . $Config->index_template );
+else $tpl = file_get_contents( '../templates/index.html' );
+
 die( $tpl );
 
 ?>
