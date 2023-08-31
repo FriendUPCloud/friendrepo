b9190bfc0f5a6931ba919e46cab451beec8a864c
Hogne Titlestad <ht@friendup.cloud>
2023-08-31 19:48:28 +0200

    Add basic structure.

---
+++ html/index.php
@@ -0,0 +1,10 @@
+<?php
+
+/*******************************************************************************
+*  This file is licensed under the MIT license. © 2023 Friend Software Labs AS *
+*******************************************************************************/
+
+$tpl = file_read_contents( '../templates/index.html' );
+die( $tpl );
+
+?>
