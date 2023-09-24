2924be9c719577e163e9d70f21ca5d6081f29d1f
Hogne Titlestad <ht@friendup.cloud>
2023-09-24 15:18:02 +0200

    Test

--- html/assets/messaging.js
+++ html/assets/messaging.js
@@ -5,12 +5,9 @@
 				let t = document.getElementsByTagName( 'a' );
 				for( let a = 0; a < t.length; a++ )
 				{
-					if( t[a].onclick )
-						t[a].onclick = null;
-					if( t[a].onmousedown )
-						t[a].onmousedown = null;
-					if( t[a].onmouseup )
-						t[a].onmouseup = null;
+					t[a].onclick = function( e ){ e.stopPropagation(); };
+					t[a].onmousedown = function( e ){ e.stopPropagation(); };
+					t[a].onmouseup = function( e ){ e.stopPropagation(); };
 					if( t[a].getAttribute( 'target' ) )
 						t[a].removeAttribute( 'target' );
 				}
