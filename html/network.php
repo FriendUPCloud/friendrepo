4e00eb5c7983d56bfed190caa986fab126b07b3a
Hogne Titlestad <ht@friendup.cloud>
2023-09-24 14:22:45 +0200

    Add some files.

---
+++ html/network.php
@@ -0,0 +1,13 @@
+<?php
+
+/*******************************************************************************
+*  This file is licensed under the MIT license. © 2023 Friend Software Labs AS *
+*******************************************************************************/
+
+/*
+	This is a network browsing proxy for Friend OS
+*/
+
+die( file_get_Contents( 'assets/404.html' ) );
+
+?>
