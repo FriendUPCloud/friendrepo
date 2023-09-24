589d7cdd2c151f745810a557763fc0a17b45b2a0
Hogne Titlestad <ht@friendup.cloud>
2023-09-24 15:13:56 +0200

    Fix sanitizer

--- html/assets/messaging.js
+++ html/assets/messaging.js
@@ -1,22 +1,25 @@
-let _friendRepo = {
-	stripATags: function()
-	{
-		let t = document.getElementsByTagName( 'a' );
-		for( let a = 0; a < t.length; a++ )
-		{
-			if( t[a].onclick )
-				t[a].onclick = null;
-			if( t[a].onmousedown )
-				t[a].onmousedown = null;
-			if( t[a].onmouseup )
-				t[a].onmouseup = null;
-			if( t[a].getAttribute( 'target' ) )
-				t[a].setAttribute( 'target', '' );
+	<script>
+		let _friendRepo = {
+			stripATags: function()
+			{
+				let t = document.getElementsByTagName( 'a' );
+				for( let a = 0; a < t.length; a++ )
+				{
+					if( t[a].onclick )
+						t[a].onclick = null;
+					if( t[a].onmousedown )
+						t[a].onmousedown = null;
+					if( t[a].onmouseup )
+						t[a].onmouseup = null;
+					if( t[a].getAttribute( 'target' ) )
+						t[a].setAttribute( 'target', '' );
+				}
+			}
 		}
-	}
-}
-document.body.addEventListener( 'load', function()
-{
-	_friendRepo.stripATags();
-} );
-_friendRepo.stripATags();
+		document.body.addEventListener( 'load', function()
+		{
+			_friendRepo.stripATags();
+		} );
+		_friendRepo.stripATags();
+	</script>
+
