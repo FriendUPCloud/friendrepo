23a98e140473c9da383ddb7d8594e04b81e42796
Hogne Titlestad <ht@friendup.cloud>
2023-09-24 17:57:34 +0200

    Test.

--- html/network.php
+++ html/network.php
@@ -70,10 +70,10 @@ function outputProxyHTML( $htmlUrl )
 		 // Remove target="_blank" attributes from links
 		$htmlContent = preg_replace( '/ target=["\']?_blank["\']?/i', '', $htmlContent );
 		$htmlContent = preg_replace( '/ target\=\"\_blank\"/i', '', $htmlContent );
-		$htmlContent = preg_replace( '/href=\"\/\//i', '/href=\"' . parse_url( $htmlUrl, PHP_URL_HOST ), $htmlContent );
-		$htmlContent = preg_replace( '/href=\"\//i', '/href=\"', $htmlContent );
-		$htmlContent = preg_replace( '/src=\"\/\//i', '/src=\"' . parse_url( $htmlUrl, PHP_URL_HOST ), $htmlContent );
-		$htmlContent = preg_replace( '/src=\"\//i', '/src=\"', $htmlContent );
+		$htmlContent = preg_replace( '/href="\/\//i', '/href="' . parse_url( $htmlUrl, PHP_URL_HOST ), $htmlContent );
+		$htmlContent = preg_replace( '/href="\//i', '/href="', $htmlContent );
+		$htmlContent = preg_replace( '/src="\/\//i', '/src="' . parse_url( $htmlUrl, PHP_URL_HOST ), $htmlContent );
+		$htmlContent = preg_replace( '/src="\//i', '/src="', $htmlContent );
 		
 		// Regular expression to match form action attributes in HTML
 		$formPattern = '/<form[^>]*action=["\']([^"\']+)["\'][^>]*>/i';
