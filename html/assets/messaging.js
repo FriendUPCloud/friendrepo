7d2aea321de7020b78fc6dc0775d95fc3f10e638
Hogne Titlestad <ht@friendup.cloud>
2023-09-24 15:13:11 +0200

    Fix

--- html/assets/messaging.js
+++ html/assets/messaging.js
@@ -1,16 +1,22 @@
+let _friendRepo = {
+	stripATags: function()
+	{
+		let t = document.getElementsByTagName( 'a' );
+		for( let a = 0; a < t.length; a++ )
+		{
+			if( t[a].onclick )
+				t[a].onclick = null;
+			if( t[a].onmousedown )
+				t[a].onmousedown = null;
+			if( t[a].onmouseup )
+				t[a].onmouseup = null;
+			if( t[a].getAttribute( 'target' ) )
+				t[a].setAttribute( 'target', '' );
+		}
+	}
+}
 document.body.addEventListener( 'load', function()
 {
-	let t = document.getElementsByTagName( 'a' );
-	for( let a = 0; a < t.length; a++ )
-	{
-		if( t[a].onclick )
-			t[a].onclick = null;
-		if( t[a].onmousedown )
-			t[a].onmousedown = null;
-		if( t[a].onmouseup )
-			t[a].onmouseup = null;
-		if( t[a].getAttribute( 'target' ) )
-			t[a].setAttribute( 'target', '' );
-	}
-	console.log( 'Set' );
+	_friendRepo.stripATags();
 } );
+_friendRepo.stripATags();
