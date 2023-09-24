9b28793d02b92b134060398ccfa6b6730a52cbee
Hogne Titlestad <ht@friendup.cloud>
2023-09-24 17:43:36 +0200

    Fix

--- html/network.php
+++ html/network.php
@@ -51,7 +51,7 @@ function outputProxyHTML( $htmlUrl )
 		$baseUrl = getBaseUrl();
 
 		// Extract the host from the base URL
-		$baseHost = parse_url( $htmlUrl, PHP_URL_SCHEME ) . '://' . parse_url( $htmlUrl, PHP_URL_HOST );
+		$baseHost = parse_url( $htmlUrl, PHP_URL_SCHEME ) . '--' . parse_url( $htmlUrl, PHP_URL_HOST );
 		
 		// Check if an existing base href tag is present in the HTML
 		if( preg_match($basePattern, $htmlContent, $matches ) ) 
@@ -132,13 +132,13 @@ function outputProxyHTML( $htmlUrl )
 	die( $modifiedHtml );
 }
 
-/*if( isset( $_REQUEST[ 'query' ] ) )
+if( isset( $_REQUEST[ 'query' ] ) )
 {
 	$uricomponent = explode( '/', $_SERVER[ 'SCRIPT_URL' ] );
 	$uricomponent = $uricomponent[2];
 	die( $uricomponent . '/' . $_REQUEST[ 'query' ] );
 	$_REQUEST[ 'url' ] = $_REQUEST[ 'query' ];
-}*/
+}
 
 if( isset( $_REQUEST[ 'url' ] ) )
 {
